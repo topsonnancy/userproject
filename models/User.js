2133ce035d6310d1762dc8008aef88191558243c
temitope fadipe <temitopefadipe@yahoo.com>
2024-02-28 12:50:00 +0100

    User Model

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    },
+    password: {
+        type: String,
+        required: true
+    },
+
+
+
+
+
+})
+
+const userModel = mongoose.model("User", userSchema)
+
+module.exports = userModel
