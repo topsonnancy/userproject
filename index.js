94da1046e6a7c57997b2ccde4187f9ab87cc7bb6
temitope fadipe <temitopefadipe@yahoo.com>
2024-02-28 11:48:18 +0100

    setting main file

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+
+
+const app = express()
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+}
+    )
